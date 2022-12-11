b3a7bffe5588452efa8a82ec3c1465d7d92c314d
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-12 02:31:53 +0900

    リファクタ

--- firstapp/react-complete-guide/src/App.js
+++ firstapp/react-complete-guide/src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+
 import logo from "./logo.svg";
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+
+const App = () => {
   const expenses = [
     {
       id: "e1",
