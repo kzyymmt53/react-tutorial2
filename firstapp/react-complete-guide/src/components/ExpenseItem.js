154408f047e794f1a97b0928afafcb204b803476
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-12 01:03:46 +0900

    css

--- firstapp/react-complete-guide/src/components/ExpenseItem.js
+++ firstapp/react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  return (
+    <div className="expense-item">
+      <div>March 28</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$297.6</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
