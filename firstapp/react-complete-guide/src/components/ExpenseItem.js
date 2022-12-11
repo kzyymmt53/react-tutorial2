11d4d7007b78d179249607e9f34519fa939abf8f
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-12 01:12:40 +0900

    javascript placeholder variable

--- firstapp/react-complete-guide/src/components/ExpenseItem.js
+++ firstapp/react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 297.6;
   return (
     <div className="expense-item">
-      <div>March 28</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$297.6</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
