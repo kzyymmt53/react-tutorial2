e1e1ffc7ab60e3e61c68c7afca544f8517aba807
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-12 00:41:26 +0900

    init

---
+++ firstapp/react-complete-guide/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>start</h2>
+      <p>visible</p>
+    </div>
+  );
+}
+
+export default App;
