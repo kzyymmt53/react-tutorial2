7db26db189ff6b89545442aeaca3143abfcd1989
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-12 00:51:11 +0900

    component

--- firstapp/react-complete-guide/src/App.js
+++ firstapp/react-complete-guide/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>start</h2>
-      <p>visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
