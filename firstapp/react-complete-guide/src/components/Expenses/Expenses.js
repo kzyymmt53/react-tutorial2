620cc5d08b17041a00ae2c560b2dc62086ab1f39
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-14 20:34:35 +0900

    expensesが空の時

--- firstapp/react-complete-guide/src/components/Expenses/Expenses.js
+++ firstapp/react-complete-guide/src/components/Expenses/Expenses.js
@@ -13,6 +13,19 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expensesContent = <p>No Expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -20,14 +33,9 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHander}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
+        
+        
       </Card>
     </div>
   );
