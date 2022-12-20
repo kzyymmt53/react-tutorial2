7f6db548ab7768cd8dd9c05bff290a1a849a73d3
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-21 01:37:06 +0900

    UsersList

--- thirdapp/01-starting-project/src/components/Users/AddUser.js
+++ thirdapp/01-starting-project/src/components/Users/AddUser.js
@@ -4,26 +4,41 @@ import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState("");
-    const [enteredAge, setEnteredAge] = useState("");
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length == 0 || enteredAge.trim().length == 0) {
+      return;
+    }
+    if (enteredAge < 1) {
+      return;
+    }
+    setEnteredUsername("");
+    setEnteredAge("");
   };
-    
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    }
-    const ageChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    }
-      return (
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <lable htmlFor="username">Username</lable>
-        <input id="username" type="text" onChange={usernameChangeHandler}></input>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        ></input>
         <lable htmlFor="age">Age (Years)</lable>
-        <input id="age" type="number"></input>
-        <Button type="submit" onChange={ageChangeHandler}>Add User</Button>
+        <input id="age" type="number" onChange={ageChangeHandler} value={enteredAge}></input>
+        <Button type="submit" >
+          Add User
+        </Button>
       </form>
     </Card>
   );
