3645774cee2cdab695921428525ef67dfc58f606
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-21 01:07:17 +0900

    CardComponent

--- thirdapp/01-starting-project/src/App.js
+++ thirdapp/01-starting-project/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import AddUser from './components/Users/AddUser';
+import Card from './components/UI/Card';
 
 function App() {
   return (
--- thirdapp/01-starting-project/src/components/Users/AddUser.js
+++ thirdapp/01-starting-project/src/components/Users/AddUser.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React from "react";
+import Card from "../UI/Card";
 
-const AddUser = props => {
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    }
-    return (
-        <form onSubmit={addUserHandler}>
-            <lable htmlFor="username">Username</lable>
-            <input id="username" type="text"></input>
-            <lable htmlFor="age">Age (Years)</lable>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
-        </form>
-    );
- };
+import classes from './AddUser.module.css';
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <lable htmlFor="username">Username</lable>
+        <input id="username" type="text"></input>
+        <lable htmlFor="age">Age (Years)</lable>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
 
 export default AddUser;
