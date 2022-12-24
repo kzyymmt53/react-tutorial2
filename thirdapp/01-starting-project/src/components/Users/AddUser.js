3d764f1436f12321b1dad2d88eb3a98da805777a
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-25 00:53:22 +0900

    modal消す

--- thirdapp/01-starting-project/src/components/Users/AddUser.js
+++ thirdapp/01-starting-project/src/components/Users/AddUser.js
@@ -7,12 +7,21 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length == 0 || enteredAge.trim().length == 0) {
+      setError({
+        title: "invalid age",
+        message: "please enter age",
+      });
       return;
     }
     if (enteredAge < 1) {
+      setError({
+        title: "invalid age",
+        message: "please enter age",
+      });
       return;
     }
 
@@ -27,9 +36,19 @@ const AddUser = (props) => {
   const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
   };
+
+  const errorHandler = () => {
+    setError(null);
+  };
   return (
     <div>
-      <ErrorModal title="error" message="something went wrong" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
