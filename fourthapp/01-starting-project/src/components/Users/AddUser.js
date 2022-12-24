5a41894ceae9424e38a9ba37b790d9f872c410cd
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-25 01:02:29 +0900

    タグをrootタグではなくリストで並べる

--- fourthapp/01-starting-project/src/components/Users/AddUser.js
+++ fourthapp/01-starting-project/src/components/Users/AddUser.js
@@ -44,35 +44,33 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
-      {error && (
-        <ErrorModal
-          title={error.title}
-          message={error.message}
-          onConfirm={errorHandler}
+    [error && (
+      <ErrorModal
+        title={error.title}
+        message={error.message}
+        onConfirm={errorHandler}
+      />
+    ),
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
         />
-      )}
-      <Card className={classes.input}>
-        <form onSubmit={addUserHandler}>
-          <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
-          <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
-          <Button type="submit">Add User</Button>
-        </form>
-      </Card>
-    </div>
-  );
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+    ]);
 };
 
 export default AddUser;
