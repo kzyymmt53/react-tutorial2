1ebc1442be1d72d488752df3969aa50ddea51918
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-25 20:02:41 +0900

    forward ref

--- sixthapp/06-usereducer-starting-project/src/components/Login/Login.js
+++ sixthapp/06-usereducer-starting-project/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -25,13 +31,15 @@ const passwordReducer = (state, action) => {
   }
   return { value: "", isValid: false };
 };
+
 const Login = (props) => {
   //const [enteredEmail, setEnteredEmail] = useState('');
   //const [emailIsValid, setEmailIsValid] = useState();
   //const [enteredPassword, setEnteredPassword] = useState('');
   //const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
-  const ctx = useContext(AuthContext);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: "",
@@ -45,6 +53,7 @@ const Login = (props) => {
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
+  const ctx = useContext(AuthContext);
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
@@ -95,13 +104,20 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    ctx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      ctx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -111,6 +127,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
@@ -120,7 +137,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
--- sixthapp/06-usereducer-starting-project/src/components/UI/Input/Input.js
+++ sixthapp/06-usereducer-starting-project/src/components/UI/Input/Input.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import classes from "./Input.module.css";
 
 const Input = (props) => {
+  const inputRef = useRef();
+  const activate = () => {
+    inputRef.current.focus();
+  };
   return (
     <div
       className={`${classes.control} ${
         props.isValid === false ? classes.invalid : ""
       }`}
     >
-          <label htmlFor={props.id}>{props.label}</label>
+      <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
