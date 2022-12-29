951d91c0ced23b8417b3d0f7f801e022cc778853
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-29 11:52:00 +0900

    カート追加

--- seventhapp/01-starting-project/src/components/Meals/MealItem/MealItem.js
+++ seventhapp/01-starting-project/src/components/Meals/MealItem/MealItem.js
@@ -1,17 +1,29 @@
+import { useContext } from 'react';
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
-  const price = `$${props.price.toFixed(2)}`;
+    const cartctx = useContext(CartContext);
+    const price = `$${props.price.toFixed(2)}`;
+    const addToCartHander = amount => { 
+        console.log(amount);
+        cartctx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price,
+        })
+    };
   return (
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+        <div className={classes.price}>{props.price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+              <MealItemForm id={props.id} onAddToCart={addToCartHander}/>
       </div>
     </li>
   );
