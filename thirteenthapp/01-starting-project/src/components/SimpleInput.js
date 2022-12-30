69f0b527ef5cc3b9c382e196caa1aed7378c6646
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-31 03:06:58 +0900

    formvalidation

--- thirteenthapp/01-starting-project/src/components/SimpleInput.js
+++ thirteenthapp/01-starting-project/src/components/SimpleInput.js
@@ -1,12 +1,55 @@
+import { useRef, useState, useEffect } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsValid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
+  const nameIputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
+  const nameIputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    setEnteredName("");
+    setEnteredNameTouched(false);
+  };
+
+  const nameInputClasses = nameInputIsValid
+    ? "form-control invalid"
+    : "form-control";
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameIputChangeHandler}
+          onBlur={nameIputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsValid && <p className="error-text">error</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
--- twelvethapp/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
+++ twelvethapp/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -1,51 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
-import useHttp from '../../hooks/use-http';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp(); 
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
 
-  const createTask = (taskData) => {
+  const createTask = (taskText, taskData) => {
     const generatedId = taskData.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
+    const createdTask = { id: generatedId, text: taskText };
 
-      props.onAddTask(createdTask);
-  }
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    sendTaskRequest({url: 'https://react-http-6b4a6.firebaseio.com/tasks.json', method: 'POST', headers: {
-      'Content-Type': 'application/json',
-    },body: { text: taskText }}, createTask);
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: "https://react-http-6b4a6.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
   };
 
   return (
