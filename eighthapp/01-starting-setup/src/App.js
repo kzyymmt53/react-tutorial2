b5ccb15983a875d131d4e87a3a6dd229fbf80dbb
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-30 22:09:29 +0900

    eight

--- eighthapp/01-starting-setup/src/App.js
+++ eighthapp/01-starting-setup/src/App.js
@@ -1,21 +1,27 @@
-import React, { useState, useCallback } from 'react';
-import BUTTON from './components/UI/Button/Button';
-import DemoOutput from './components/Demo/DemoOutput';
+import React, { useState, useCallback } from "react";
+import BUTTON from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
-
-
-import './App.css';
+import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      <BUTTON onClick={allowToggleHandler}>toggle paragraph</BUTTON>
       <BUTTON onClick={toggleParagraphHandler}>toggle paragraph</BUTTON>
     </div>
   );
