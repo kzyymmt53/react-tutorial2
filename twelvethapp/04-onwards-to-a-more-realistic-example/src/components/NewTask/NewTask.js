52a262a8b4f9360dc2aebd461c98aabf0c146f36
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-31 02:01:07 +0900

    usehttp使い方

--- twelvethapp/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
+++ twelvethapp/04-onwards-to-a-more-realistic-example/src/components/NewTask/NewTask.js
@@ -2,12 +2,22 @@ import { useState } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp(); 
+
+  const createTask = (taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+  }
 
   const enterTaskHandler = async (taskText) => {
+    sendTaskRequest({url: 'https://react-http-6b4a6.firebaseio.com/tasks.json', method: 'POST', headers: {
+      'Content-Type': 'application/json',
+    },body: { text: taskText }}, createTask);
     setIsLoading(true);
     setError(null);
     try {
