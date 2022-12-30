8e6326b32f0226188f9a07e913dc90a0dfe7c062
kzyymmt53 <kzyymmt53@gmail.com>
2022-12-30 22:42:11 +0900

    loading

--- ninethapp/01-starting-project/src/App.js
+++ ninethapp/01-starting-project/src/App.js
@@ -5,9 +5,13 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
-    const data = response.json();
+    const data = await response.json();
+    console.log(data.results);
 
     const transformedMovies = data.results.map((movieData) => {
       return {
@@ -18,6 +22,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
