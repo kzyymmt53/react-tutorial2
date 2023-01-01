0d1b6604ba0d61460e88bc5054ea9d47eb8d259a
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-02 03:15:49 +0900

    カートをput

--- sixteenthapp/01-starting-project/src/App.js
+++ sixteenthapp/01-starting-project/src/App.js
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+let initial = true;
+
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    if (initial) {
+      initial = false;
+      return;
+    }
+    //カートの中身をバックエンドに送る
+   }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
