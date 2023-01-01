d31093ecf047893ee0d557aaa7c18dfad0e191a3
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-01 19:07:27 +0900

    fetchmeals

--- fourteenth/01-starting-project/src/components/Meals/AvailableMeals.js
+++ fourteenth/01-starting-project/src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
+import { useEffect, useState } from 'react';
 
 const DUMMY_MEALS = [
   {
@@ -30,7 +31,29 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch("url").then(() => { });
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in loadedMeals) {
+        loadedMeals.push({
+          id: key,
+          name: response[key].name,
+          description: response[key].description,
+          price: responseData[key].price
+
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+    
+  }, []);
+  
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
