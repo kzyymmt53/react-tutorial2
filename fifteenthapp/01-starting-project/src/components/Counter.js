8eae542a954715a0cfe413db11fa3425ef9e2d63
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-01 22:22:30 +0900

    ReduxToolkit使い方

--- fifteenthapp/01-starting-project/src/components/Counter.js
+++ fifteenthapp/01-starting-project/src/components/Counter.js
@@ -2,24 +2,26 @@ import { Component } from 'react';
 import { useSelector, useDispatch, connect } from "react-redux";
 import classes from "./Counter.module.css";
 
+import { counterActions } from '../store/index';
+
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
   const show = useSelector(state => state.showCounter);
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggleCounter());
    };
   
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 });
+    dispatch(counterActions.increase(5));
   }
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
   return (
     <main className={classes.counter}>
