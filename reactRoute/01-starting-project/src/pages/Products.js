a592d8a4c31f264179bd417922a85bc9cc3ee3b8
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-02 14:09:37 +0900

    全てのマッチしたRouteが表示されないようにするにはSwitchで囲む

--- reactRoute/01-starting-project/src/pages/Products.js
+++ reactRoute/01-starting-project/src/pages/Products.js
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 const Products = () => {
   return (
     <section>
       <h1>Products</h1>
       <ul>
-        <li>Book</li>
-        <li>Carpet</li>
-        <li>Online Course</li>
+        <li><Link to="/products/p1">Book</Link></li>
+        <li><Link to="/products/p2">Carpet</Link></li>
+        <li><Link to="/products/p3">Online Course</Link></li>
       </ul>
     </section>
   );
