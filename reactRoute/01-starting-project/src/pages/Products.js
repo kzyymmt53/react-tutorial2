f99526c58781482fbd1ec6806282b58ef491307e
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-02 14:00:02 +0900

    ダイナミックroute :id

--- reactRoute/01-starting-project/src/pages/Products.js
+++ reactRoute/01-starting-project/src/pages/Products.js
@@ -1,5 +1,14 @@
 const Products = () => {
-    return <h1>Products</h1>;
-}
+  return (
+    <section>
+      <h1>Products</h1>
+      <ul>
+        <li>Book</li>
+        <li>Carpet</li>
+        <li>Online Course</li>
+      </ul>
+    </section>
+  );
+};
 
 export default Products;
