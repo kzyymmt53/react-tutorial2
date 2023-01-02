4e7dba670f3ccf87e799566efaf51bff368b8087
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-02 15:52:38 +0900

    URLごとに違うコンポーネントを出すときにネスと

--- reactRouteSampleApp/09-time-to-practice-starting-code/src/pages/QuateDetail.js
+++ reactRouteSampleApp/09-time-to-practice-starting-code/src/pages/QuateDetail.js
@@ -1,31 +1,38 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 import Comments from "../components/comments/Comments";
 
 const DUMMY_QUATES = [
-    { id: 'q1', author: 'Max', text: "Learing React is fun" },
-    { id: 'q2', author: 'Maxi', text: "Learing React is Great" },
+  { id: "q1", author: "Max", text: "Learing React is fun" },
+  { id: "q2", author: "Maxi", text: "Learing React is Great" },
 ];
 
 const QuateDetail = () => {
+  const param = useParams();
 
-    const param = useParams();
-    
-    const quote = DUMMY_QUATES.find(quote => quote.id === param.quiteId);
+  const quote = DUMMY_QUATES.find((quote) => quote.id === param.quiteId);
 
-    if (!quote) {
-        return <p>No quote found</p>;
-    }
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
 
   return (
     <Fragment>
-          <HighlightedQuote text={quote.text} author={quote.author} />
-          <Route path={`/quates/${param.quiteId}/comments`}>
-            <Comments />
-          </Route>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quates/${param.quiteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quates/${param.quiteId}/comments`}>
+            Load comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`/quates/${param.quiteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
