73349382743d4581f80ace10ad97eb9d146ba22d
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-29 20:53:00 +0900

    test

--- unittest/01-starting-project/src/components/Greeting.js
+++ unittest/01-starting-project/src/components/Greeting.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
     const [changedText, setChangeText] = useState(false);
@@ -9,7 +10,7 @@ const Greeting = () => {
     return (
         <div>
             <h2>Hello World!</h2>
-            {!changedText && <p>It's good to see you!</p>}
+            {!changedText && <Output>It's good to see you!</Output>}
             {changedText && <p>Changed!</p>}
             <button onClick={changeTextHandler}>Change Text!</button>
         </div>
