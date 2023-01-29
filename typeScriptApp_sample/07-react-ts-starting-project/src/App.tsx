2e3d9489a8b95c6cc6d97caf9be48bd060ec7444
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-29 22:50:32 +0900

    typescriptcomponent

--- typeScriptApp_sample/07-react-ts-starting-project/src/App.tsx
+++ typeScriptApp_sample/07-react-ts-starting-project/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo("Lear React"),
+    new Todo("Learn Typescript"),
+  ];
   return (
     <div>
-      <Todos items={["Lear React", "Learn Typescript"] } />
+      <Todos items={todos} />
     </div>
   );
 }
