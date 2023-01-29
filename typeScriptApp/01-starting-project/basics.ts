551945063160272c0b682beda3777046dccfefbf
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-29 22:05:37 +0900

    Generics

--- typeScriptApp/01-starting-project/basics.ts
+++ typeScriptApp/01-starting-project/basics.ts
@@ -37,3 +37,12 @@ function add(a: number, b: number): number {
 function print(value: any) {
     console.log(value);
 }
+
+//Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1);
