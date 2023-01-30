732b109bb6a0059d89a81a4892356abd731d8eda
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-31 00:35:13 +0900

    TodoItems

--- typeScriptApp_sample/07-react-ts-starting-project/src/components/Todos.tsx
+++ typeScriptApp_sample/07-react-ts-starting-project/src/components/Todos.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Todo from "../models/todo";
-const Todos: React.FC<{items: Todo[]}> = (props) => {
-    return <ul>
-        {props.items.map(item => <li key={item.id}>{item.text}</li>)}
-    </ul>;
-}
+import TodoItem from "./TodoItem";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
+    </ul>
+  );
+};
 
 export default Todos;
