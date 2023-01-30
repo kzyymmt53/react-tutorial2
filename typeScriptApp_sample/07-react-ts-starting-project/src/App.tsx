76cf2d3248034ca7c1787e90cf41657ad9b14390
kzyymmt53 <kzyymmt53@gmail.com>
2023-01-31 01:06:11 +0900

    TypescriptでuseState

--- typeScriptApp_sample/07-react-ts-starting-project/src/App.tsx
+++ typeScriptApp_sample/07-react-ts-starting-project/src/App.tsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [
-    new Todo("Lear React"),
-    new Todo("Learn Typescript"),
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => { };
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodo) => { 
+      return prevTodo.concat(newTodo);
+    });
+   };
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler}/>
